68949f3422737ca2cb3f1ec1d739e6418f45880d
Ilya Erlingas <Aligruit@gmail.com>
2019-12-17 21:17:47 +0300

    2 nd commit

--- JsonHW.py
+++ JsonHW.py
@@ -11,12 +11,21 @@ data_json = data["rss"]
 channel_description = data_json["channel"]["description"].split(" ")
 channel_category = data_json["channel"]["category"].split(" ")
 channel_title = data_json["channel"]["title"].split(" ")
+channel_item = data_json["channel"]["items"]
 channel = {}
-for i in channel_description:
-    if len(i) >= 6: print(i)
-    for k in channel_category:
-        if len(k) >= 6: print(k)
-        for g in channel_title:
-            if len(g) >= 6: print(g)
-
-    # print(key, end="")
+for ch_dis in channel_description:
+    if len(ch_dis) >= 6: print(ch_dis)
+    for ch_cat in channel_category:
+        if len(ch_cat) >= 6: print(ch_cat)
+        for ch_t in channel_title:
+            if len(ch_t) >= 6: print(ch_t)
+            for ch_item in channel_item:
+                splited_ch_item_description = ch_item["description"].split(" ")
+                for ch_item_split in splited_ch_item_description:
+                    if len(ch_item_split) >= 6:
+                        print(ch_item_split)
+                for ch_item_ in channel_item:
+                    splited_ch_item_title = ch_item_["title"].split(" ")
+                    for splited_title in splited_ch_item_title:
+                        if len(splited_title) >=6:
+                            print(splited_title)
